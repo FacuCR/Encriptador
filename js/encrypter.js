5115059389314406a58d55c911c72e8f47590cb2
FacuCR <facucastro302@gmail.com>
2022-05-23 22:37:01 -0300

    script de desencriptar, mostrar mensaje desencriptado en vista listo

--- js/encrypter.js
+++ js/encrypter.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 const inputMessage = document.querySelector("textarea");
 const buttonEncrypter = document.querySelector(".btn-encrypter");
 const buttonDesencrypter = document.querySelector(".btn-desencrypter");
@@ -6,9 +6,15 @@ const outputContent = document.querySelector(".content");
 let outputText = "";
 
 buttonEncrypter.addEventListener("click", () => {
-    outputText = encrypter(inputMessage.value);
-    deleteChildrenElements(outputContent);
-    addMessageToOutputSection(outputContent, outputText);
+  outputText = encrypter(inputMessage.value);
+  deleteChildrenElements(outputContent);
+  addMessageToOutputSection(outputContent, outputText);
+});
+
+buttonDesencrypter.addEventListener("click", () => {
+  outputText = desencrypter(inputMessage.value);
+  deleteChildrenElements(outputContent);
+  addMessageToOutputSection(outputContent, outputText);
 });
 
 function encrypter(message) {
@@ -42,13 +48,115 @@ function encrypter(message) {
   return encryptedMessage;
 }
 
+function desencrypter(message) {
+  let desencryptedMessage = message;
+
+  desencryptedMessage = desencryptAOfTheMessage(desencryptedMessage);
+  desencryptedMessage = desencryptEOfTheMessage(desencryptedMessage);
+  desencryptedMessage = desencryptIOfTheMessage(desencryptedMessage);
+  desencryptedMessage = desencryptOOfTheMessage(desencryptedMessage);
+  desencryptedMessage = desencryptUOfTheMessage(desencryptedMessage);
+
+  return desencryptedMessage;
+}
+
+function desencryptAOfTheMessage(message) {
+  let words;
+  let desencryptedOnlyAMessage = "";
+
+  for (let i = 0; i < message.length; i++) {
+    words = message[i] + message[i + 1];
+    if (words === "ai") {
+      desencryptedOnlyAMessage += "a";
+      i++;
+    } else {
+      desencryptedOnlyAMessage += message[i];
+    }
+  }
+
+  return desencryptedOnlyAMessage;
+}
+
+function desencryptEOfTheMessage(message) {
+  let words;
+  let desencryptedOnlyEMessage = "";
+
+  for (let i = 0; i < message.length; i++) {
+    words =
+      message[i] +
+      message[i + 1] +
+      message[i + 2] +
+      message[i + 3] +
+      message[i + 4];
+    if (words === "enter") {
+      desencryptedOnlyEMessage += "e";
+      i += 4;
+    } else {
+      desencryptedOnlyEMessage += message[i];
+    }
+  }
+
+  return desencryptedOnlyEMessage;
+}
+
+function desencryptIOfTheMessage(message) {
+  let words;
+  let desencryptedOnlyIMessage = "";
+
+  for (let i = 0; i < message.length; i++) {
+    words = message[i] + message[i + 1] + message[i + 2] + message[i + 3];
+    if (words === "imes") {
+      desencryptedOnlyIMessage += "i";
+      i += 3;
+    } else {
+      desencryptedOnlyIMessage += message[i];
+    }
+  }
+
+  return desencryptedOnlyIMessage;
+}
+
+function desencryptOOfTheMessage(message) {
+  let words;
+  let desencryptedOnlyOMessage = "";
+
+  for (let i = 0; i < message.length; i++) {
+    words = message[i] + message[i + 1] + message[i + 2] + message[i + 3];
+    if (words === "ober") {
+      desencryptedOnlyOMessage += "o";
+      i += 3;
+    } else {
+      desencryptedOnlyOMessage += message[i];
+    }
+  }
+
+  return desencryptedOnlyOMessage;
+}
+
+function desencryptUOfTheMessage(message) {
+  let words;
+  let desencryptedOnlyUMessage = "";
+
+  for (let i = 0; i < message.length; i++) {
+    words = message[i] + message[i + 1] + message[i + 2] + message[i + 3];
+    if (words === "ufat") {
+      desencryptedOnlyUMessage += "u";
+      i += 3;
+    } else {
+      desencryptedOnlyUMessage += message[i];
+    }
+  }
+
+  return desencryptedOnlyUMessage;
+}
+
 function deleteChildrenElements(elementHTML) {
-    elementHTML.innerHTML = "";
+  elementHTML.innerHTML = "";
 }
 
 function addMessageToOutputSection(elementHTML, message) {
-    const p = document.createElement('p');
-    const text = document.createTextNode(message);
-    p.appendChild(text);
-    elementHTML.appendChild(p);
+  const p = document.createElement("p");
+  const text = document.createTextNode(message);
+  p.appendChild(text);
+  elementHTML.appendChild(p);
 }
